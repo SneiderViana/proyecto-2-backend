bb29b54aa4516c969a539976d9e1f955ae65ca3e
SneiderViana <sdviana@unicesar.edu.co>
2024-11-08 17:28:02 -0500

    subida de proyecto

---
+++ src/main/java/proyecto_2_backend/proyecto_2_backend/Proyecto2BackendApplication.java
@@ -0,0 +1,13 @@
+package proyecto_2_backend.proyecto_2_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto2BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto2BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/proyecto_2_backend/proyecto_2_backend/Proyecto2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package proyecto_2_backend.proyecto_2_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto2BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
