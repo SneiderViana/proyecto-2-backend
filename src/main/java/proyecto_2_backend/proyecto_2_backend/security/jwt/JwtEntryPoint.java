9e037e12ede139c732f46d0be02e8d8f1b5b5d22
SneiderViana <sdviana@unicesar.edu.co>
2024-11-09 19:23:56 -0500

    avance jwt

---
+++ src/main/java/proyecto_2_backend/proyecto_2_backend/security/jwt/JwtEntryPoint.java
@@ -0,0 +1,27 @@
+package proyecto_2_backend.proyecto_2_backend.security.jwt;
+
+
+import java.io.IOException;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtEntryPoint implements AuthenticationEntryPoint{
+
+    private static final org.slf4j.Logger logger= LoggerFactory
+                    .getLogger(JwtEntryPoint.class);
+    
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+            AuthenticationException authException) throws IOException, ServletException {
+                logger.error("token not found or invalid");
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "unauthorized");
+    }
+}
+
+  
